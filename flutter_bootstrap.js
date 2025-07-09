695a029bea283d55a2c812dd0f6074a9e732e041
David Bryan Ramboy <davidbryanramboy@gmail.com>
2025-07-10 01:27:54 +0800

    push fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"010c8a806bccad64ab9972286b85dd24ca9844
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "340503100"
+    serviceWorkerVersion: "2196744858"
   }
 });
